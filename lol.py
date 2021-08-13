8a5fe434bb49ec58b9a64b0f744e1e03af6eea8b
Như Bảo Trương <28810481+nhubaotruong@users.noreply.github.com>
2021-08-13 22:12:53 +0700

    Kill old LOL process before starting, add more debug text

--- lol.py
+++ lol.py
@@ -2,8 +2,10 @@ import psutil
 import os
 import yaml
 import sqlite3
+import re
 
-def findProcessIdByName(processName):
+
+def find_process_id_by_name(processName):
     """
     Get a list of all the PIDs of a all the running process whose name contains
     the given string processName
@@ -19,23 +21,43 @@ def findProcessIdByName(processName):
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
 
+
+def kill_old_league_and_riot_process():
+    RIOT_AND_LEAGUE_PROCESS_REGEX = re.compile("(league|riot).*\.exe", flags=re.I)
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=["name", "pid"])
+            # Check if process name contains the given name string.
+            if RIOT_AND_LEAGUE_PROCESS_REGEX.search(pinfo.get("name")):
+                print(
+                    f"Found old process still running: {pinfo.get('name')}, pid: {pinfo.get('pid')}"
+                )
+                p = psutil.Process(pinfo.get("pid"))
+                p.kill()
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+
 def main():
     riot_service_process_name = "RiotClientServices.exe"
 
+    print(f"The script will wait for a {riot_service_process_name} process to show up to get it's token. So go to the Garena client and press Play")
+    kill_old_league_and_riot_process()
+
     # Get RiotClientServices.exe process
-    riot_process = findProcessIdByName(riot_service_process_name)
+    riot_process = find_process_id_by_name(riot_service_process_name)
 
     # Programmatically get RiotClientServices.exe arguments
     try:
         riot_argument = " ".join(riot_process.get("cmdline", [])[1:]).strip()
     except Exception:
         print("Cannot get Garena Token")
-    
+
     # Exit if no riot_argument is found
     if not riot_argument:
         quit()
 
-    print(riot_argument)
+    print(f"Garena token got from {riot_service_process_name} {riot_argument}")
 
     # Kill the current RiotClientServices.exe
     p = psutil.Process(riot_process.get("pid"))
@@ -44,8 +66,13 @@ def main():
     # Start game using lutris
     HOME = os.getenv("HOME")
     LUTRIS_CONFIG_FOLDER = HOME + "/.config/lutris/games/"
-    LUTRIS_LOL_CONFIG_FILE = LUTRIS_CONFIG_FOLDER + os.popen(f'ls {LUTRIS_CONFIG_FOLDER} | grep -i -E league.*.yml').read().strip()
-    
+    LUTRIS_LOL_CONFIG_FILE = (
+        LUTRIS_CONFIG_FOLDER
+        + os.popen(f"ls {LUTRIS_CONFIG_FOLDER} | grep -i -E league.*.yml")
+        .read()
+        .strip()
+    )
+
     with open(LUTRIS_LOL_CONFIG_FILE, "r") as f:
         try:
             game_config = yaml.load(f, Loader=yaml.FullLoader)
@@ -59,24 +86,32 @@ def main():
             yaml.dump(game_config, f, default_flow_style=False)
         except yaml.YAMLError as exc:
             print(exc)
-    
+
     LUTRIS_DB_PATH = HOME + "/.local/share/lutris"
     LUTRIS_DB_NAME = "pga.db"
     conn = sqlite3.connect(LUTRIS_DB_PATH + "/" + LUTRIS_DB_NAME)
     c = conn.cursor()
     c.execute("SELECT id FROM games WHERE slug='league-of-legends'")
     try:
-        game_id =  c.fetchone()[0]
+        game_id = c.fetchone()[0]
         conn.close()
     except Exception:
         print("No league of legends install from lutris found")
         conn.close()
         quit()
-    
-    print(f"lutris game id: {game_id}")
+
+    # print(f"lutris game id: {game_id}")
+    print("Starting game with current config:")
+    print(f"- Wine version: {game_config.get('wine',{}).get('version')}")
+    print(f"- Executable: {game_config.get('game',{}).get('exe')}")
+    print(f"- Wineprefix: {game_config.get('game',{}).get('prefix')}")
+
+    print("If this is your first time running LOL since reboot, a pop up will appear, chose the first or second option then enter your password\n")
+    print("It's a workaround by the lutris community\n")
+    print("Ignore the lines below, they are not actually error\n")
     launch_command = f"lutris lutris:rungameid/{game_id}"
     os.system(launch_command)
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
