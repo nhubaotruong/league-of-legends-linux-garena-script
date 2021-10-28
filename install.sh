2e155e3e48928c769859c5f45c5eccb06f9b358b
nhubao <baoprokute1234@gmail.com>
2021-10-28 11:28:28 +0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Install Garena
+env LUTRIS_SKIP_INIT=1 lutris -i https://raw.githubusercontent.com/nhubaotruong/league-of-legends-linux-garena-script/main/garena.json
+# Install LOL NA
+env LUTRIS_SKIP_INIT=1 lutris -i https://lutris.net/api/installers/league-of-legends-standard-launch-help?format=json
