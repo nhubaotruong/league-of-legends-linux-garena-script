f5cc48bc9d43bc07007edb4b8d970db335d12275
nhubao <baoprokute1234@gmail.com>
2021-11-13 11:55:26 +0700

    Fix config script

--- config.py
+++ config.py
@@ -10,15 +10,32 @@ def main():
     LUTRIS_DB_NAME = "pga.db"
     conn = sqlite3.connect(LUTRIS_DB_PATH + "/" + LUTRIS_DB_NAME)
     c = conn.cursor()
-    c.execute("SELECT configpath,directory FROM games WHERE slug='garena'")
+    c.execute("SELECT directory FROM games WHERE slug='garena'")
     try:
-        configpath, directory = c.fetchone()
+        directory = c.fetchone()[0]
         conn.close()
+    except Exception:
+        print("No Garena install from lutris found")
+        conn.close()
+        quit()
+    exec_path = ""
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file == riot_service_process_name:
+                exec_path = os.path.join(root, file)
+    if not exec_path:
+        print("League of legends cannot be found in Garena folder")
+        quit()
+
+    conn = sqlite3.connect(LUTRIS_DB_PATH + "/" + LUTRIS_DB_NAME)
+    c = conn.cursor()
+    c.execute("SELECT configpath FROM games WHERE slug='league-of-legends'")
+    try:
+        configpath = c.fetchone()[0]
     except Exception:
         print("No league of legends install from lutris found")
         conn.close()
         quit()
-
     LUTRIS_CONFIG_FOLDER = HOME + "/.config/lutris/games/"
     LUTRIS_LOL_CONFIG_FILE = LUTRIS_CONFIG_FOLDER + configpath + ".yml"
 
@@ -30,14 +47,6 @@ def main():
             quit()
 
     with open(LUTRIS_LOL_CONFIG_FILE, "w") as f:
-        exec_path = ""
-        for root, _, files in os.walk(directory):
-            for file in files:
-                if file == riot_service_process_name:
-                    exec_path = os.path.join(root, file)
-        if not exec_path:
-            print("League of legends cannot be found in Garena folder")
-            quit()
         try:
             game_config['game']['exe'] = exec_path
             yaml.dump(game_config, f, default_flow_style=False)
